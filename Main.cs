44c4572fa315af8d3a91a17ca4f2698b4e97f8bd
Zemogiter <zumi999@vp.pl>
2023-01-02 19:45:54 +0100

    Initial update to BepInEx

--- Main.cs
+++ Main.cs
@@ -1,24 +1,15 @@
 ﻿using HarmonyLib;
-using QModManager.API.ModLoading;
-using QModManager.API;
+using BepInEx;
 using System.Reflection;
 
 namespace No_Feedback_Reminder
 {
-    // Your main patching class must have the QModCore attribute (and must be public)
-    [QModCore]
-    public static class Main
+    [BepInPlugin("com.zemogiter.nofeedbackreminder", "NoFeedbackReminder", "1.0.0.0")]
+    public class MyPlugin : BaseUnityPlugin
     {
-        // Your patching method must have the QModPatch attribute (and must be public)
-        [QModPatch]
-        public static void Patch()
+        private void Start()
         {
-            // Add your patching code here
-            //QModServices.Main.AddCriticalMessage("No Feedback Reminder loaded.");
-#if DEBUG
-            Logger.Log("No Feedback Reminder loaded.");
-#endif
-            Harmony harmony = new Harmony("com.yw2theorycrafter.nofeedbackreminder");
+            var harmony = new Harmony("com.zemogiter.nofeedbackreminder");
             harmony.PatchAll(Assembly.GetExecutingAssembly());
         }
     }
